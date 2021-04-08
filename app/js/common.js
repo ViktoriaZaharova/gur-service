0ed60417fe51cfdbf7e972f91eb9f927cb68682b
Viktoria <zaharova181114@gmail.com>
2021-04-08 23:58:13 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+$('[name="phone"]').mask('+7 (999) 999-99-99');
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+new WOW().init();
